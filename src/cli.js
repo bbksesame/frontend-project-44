477eee33f71576b7709f1b1c2ac79a03379afca9
bbksesame <bbk-sesame@yandex.ru>
2023-02-18 18:35:26 +0600

    fix

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const nameCheck = (text) => {
 console.log('Welcome to the Brain Games!')
-const name = readlineSync.question('May I have your name?: ');
-return text + name
+const userName = readlineSync.question('May I have your name?: ');
+return text + userName + '!'
 }
 export default nameCheck
