456e61d1f8ddd19c038d4f894f382f7edf51116a
bbksesame <bbk-sesame@yandex.ru>
2023-02-25 03:37:20 +0600

    fix

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,29 +1,47 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-import arifmetProg from '../../frontend-project-44/src/arifmetProg.js'
 import getRandomInt from '../../frontend-project-44/src/random.js'
-import del from '../../frontend-project-44/src/delitel.js'
-import rand from '../../frontend-project-44/src/randOper.js'
-import math_it_up from '../../frontend-project-44/src/math.js'
+import arifmetProg from '../../frontend-project-44/src/arifmetProg.js'
+
 
 
 console.log('Welcome to the Brain Games!')
 const userName = readlineSync.question('May I have your name?: ');
 console.log('Hello, ' + userName + '!')
 console.log('What number is missing in the progression?')
-let i = 0
-loop: while (i < 3) {
-var randomNum = getRandomInt(2,80)
-var randomNums = getRandomInt(5,10)
-var randomPos = getRandomInt(1,9)
+let k = 0
+loop: while (k < 3) {
+    var arrProg = []
 var arrTest = []
-const number = readlineSync.question(`Question: ${arifmetProg(randomNum, randomNums)}\nYour answer: `);
-if (+number === arrTest[randomPos]) {
-    console.log('Correct!')
-    i ++
-    continue
-} if (+number === 'q') {
+var randomStart = getRandomInt(2,10)
+var randomStep2 = getRandomInt(2,10)
+var randomPos = getRandomInt(1,9)
+var randomNum = getRandomInt(2,80)
+var randomNums = getRandomInt(6,10)
+
+
+    const arifmetProg = (num,numProg) => {
+        for (let i = 0; i < numProg; i++) {
+          arrTest.push(num + randomStart)
+          randomStart = randomStart +  randomStep2
+        } 
+       arrProg = arrTest.slice()
+        arrProg[randomPos] = '..'
+        return arrProg
+      }
+      var go = arifmetProg(randomNum, randomNums)
+
+const number = readlineSync.question(`Question: ${go}\nYour answer: `);
+if (+number !== arrTest[randomPos]) {
+    console.log(`'${number}' is wrong answer ;(. Correct answer was '${arrTest[randomPos]}'. \nLet's try again, ${userName}!`)
     break loop
+} if (+number === arrTest[randomPos]) {
+    console.log('Correct!')
+    k++
+    continue
+} 
 }
+if (k === 3) {
+    console.log('Congratulations, ' + userName + '!')
 }
