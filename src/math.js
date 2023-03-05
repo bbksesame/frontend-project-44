e3331d0a0685ef24b305414d144d233eb2853f6e
bbksesame <115954782+bbksesame@users.noreply.github.com>
2023-03-05 21:51:25 +0600

    Delete src directory

--- src/math.js
+++
@@ -1,8 +0,0 @@
-const math_it_up = {
-    '+': function (x, y) { return x + y },
-    '-': function (x, y) { return x - y },
-    '*': function (x, y) { return x * y }
-}
-
-
-export default math_it_up
--- src/randOper.js
+++
@@ -1,5 +0,0 @@
-var znaki = ['-', '+', '*']
-var rand = Math.floor(Math.random() * znaki.length);
-var znak = znaki[rand]
-
-export default rand
