1bb320abbbff9b5d0e384860caca1a79e18a4210
bbksesame <115954782+bbksesame@users.noreply.github.com>
2023-03-05 21:51:17 +0600

    Delete cli.js

--- src/cli.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const nameCheck = (text) => {
-console.log('Welcome to the Brain Games!')
-const userName = readlineSync.question('May I have your name?: ');
-return text + userName + '!'
-}
-export default nameCheck
